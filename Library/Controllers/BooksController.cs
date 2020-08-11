689cc91faa5b71d1f4d42381b68c7b34838b9a35
Kate Skorija <kateskorija@gmail.com>
2020-08-11 11:58:37 -0700

    create books controller routes
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,151 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Library.Models;
+
+namespace Library.Controllers
+{
+  [Authorize]
+  public class BooksController : Controller
+  {
+    private readonly LibraryContext _db;
+    private readonly UserManager<LibrarianUser> _userManager;
+
+    public BooksController(UserManager<LibrarianUser> userManager, LibraryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Books.OrderBy(book => book.Title).ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.GenreId = new SelectList(_db.Genres, "GenreId", "Name");
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "FullName");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Book book, int AuthorId, int GenreId)
+    {
+      _db.Books.Add(book);
+      if (GenreId != 0)
+      {
+        _db.BookGenre.Add(new BookGenre() { GenreId = GenreId, BookId = book.BookId });
+      }
+      if (AuthorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() { AuthorId = AuthorId, BookId = book.BookId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      var thisBook = _db.Books
+          .Include(book => book.Genres)
+          .ThenInclude(join => join.Genre)
+          .Include(book => book.Authors)
+          .ThenInclude(join => join.Author)
+          .FirstOrDefault(book => book.BookId == id);
+      return View(thisBook);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(books => books.BookId == id);
+      ViewBag.GenreId = new SelectList(_db.Genres, "GenreId", "Name");
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "FullName");
+      return View(thisBook);
+    }
+    [HttpPost]
+    public ActionResult Edit(Book book, int AuthorId, int GenreId)
+    {
+      if (GenreId != 0)
+      {
+        _db.BookGenre.Add(new BookGenre() { GenreId = GenreId, BookId = book.BookId });
+      }
+      if (AuthorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() { AuthorId = AuthorId, BookId = book.BookId });
+      }
+      _db.Entry(book).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddGenre(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      ViewBag.GenreId = new SelectList(_db.Genres, "GenreId", "Name");
+      return View(thisBook);
+    }
+
+    [HttpPost]
+    public ActionResult AddGenre(Book book, int GenreId)
+    {
+      if (GenreId != 0)
+      {
+        _db.BookGenre.Add(new BookGenre() { GenreId = GenreId, BookId = book.BookId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult AddAuthor(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "Name");
+      return View(thisBook);
+    }
+
+    [HttpPost]
+    public ActionResult AddAuthor(Book book, int AuthorId)
+    {
+      if (AuthorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() { AuthorId = AuthorId, BookId = book.BookId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      return View(thisBook);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      _db.Books.Remove(thisBook);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public ActionResult DeleteGenre(int joinId)
+    {
+      var joinEntry = _db.BookGenre.FirstOrDefault(entry => entry.BookGenreId == joinId);
+      _db.BookGenre.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public ActionResult DeleteAuthor(int joinId)
+    {
+      var joinEntry = _db.AuthorBook.FirstOrDefault(entry => entry.AuthorBookId == joinId);
+      _db.AuthorBook.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
